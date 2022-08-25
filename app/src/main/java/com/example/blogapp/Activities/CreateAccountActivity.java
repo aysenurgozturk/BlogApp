55e5ed046f309b42c02f459e6fed32328b3379c3
aysenurgozturk <aysenurgozturkk@gmail.com>
2022-08-25 12:21:43 +0300

    new features added

--- app/src/main/java/com/example/blogapp/Activities/CreateAccountActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/CreateAccountActivity.java
@@ -1,16 +1,24 @@
 package com.example.blogapp.Activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.blogapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +28,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.BreakIterator;
 
@@ -33,9 +44,32 @@ public class CreateAccountActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private FirebaseAuth mAuth;
     private ProgressDialog mProgressDialog;
+    private ImageButton profilePic;
+    private Uri mImageUri = null;
+    private StorageReference mFirebaseStorage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult activityResult) {
+                            int result = activityResult.getResultCode();
+                            Intent data = activityResult.getData();
+
+                            if (result == RESULT_OK) {
+                                String title = data.getStringExtra("title");
+                                setTitle(title);
+                                Toast.makeText(getApplicationContext(), "Title modified", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Operation canceled", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+            );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
@@ -44,6 +78,9 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        mFirebaseStorage = FirebaseStorage.getInstance().getReference().child("MBlog_Profile_Pics");
+
+
         mProgressDialog = new ProgressDialog(this);
 
         firstname = findViewById(R.id.firstname);
@@ -51,6 +88,18 @@ public class CreateAccountActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         button = findViewById(R.id.button);
+        profilePic = findViewById(R.id.profilePic);
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                activityResultLauncher.launch(galleryIntent);
+
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,31 +125,36 @@ public class CreateAccountActivity extends AppCompatActivity {
             mProgressDialog.setMessage("Creating Account...");
             mProgressDialog.show();
 
-            Log.d("message",em+pwd);
-
             mAuth.createUserWithEmailAndPassword(em, pwd)
                     .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
 
                         @Override
                         public void onSuccess(AuthResult authResult) {
-                            Log.d("message","1");
                             if (authResult != null) {
-                                Log.d("message","2");
 
-                                String userid = mAuth.getCurrentUser().getUid();
-                                DatabaseReference currenUserDb = mDatabaseReference.child(userid);
-                                currenUserDb.child("firstname").setValue(name);
-                                currenUserDb.child("lastname").setValue(lname);
-                                currenUserDb.child("image").setValue("none");
+                                StorageReference imagePath = mFirebaseStorage.child("MBlog_Profile_Pics")
+                                        .child(mImageUri.getLastPathSegment());
+                                Log.d("selam","1");
+                                imagePath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        Log.d("selam","2");
+                                        String userid = mAuth.getCurrentUser().getUid();
+                                        DatabaseReference currenUserDb = mDatabaseReference.child(userid);
+                                        currenUserDb.child("firstname").setValue(name);
+                                        currenUserDb.child("lastname").setValue(lname);
+                                        currenUserDb.child("image").setValue("mImageUri.toString()");
+                                        Log.d("selam","3");
+                                        mProgressDialog.dismiss();
 
-                                Log.d("message","3");
+                                        //send users to postList
+                                        Intent intent = new Intent(CreateAccountActivity.this, PostListActivity.class );
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                        startActivity(intent);
+                                    }
+                                });
 
-                                mProgressDialog.dismiss();
 
-                                //send users to postList
-                                Intent intent = new Intent(CreateAccountActivity.this, PostListActivity.class );
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                startActivity(intent);
 
                                     }
                                 }
@@ -109,5 +163,15 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+         mImageUri = data.getData();
+        profilePic.setImageURI(mImageUri);
+        }
     }
 }
